1ac14126784cb2742c2f37ff1aa62f5e34c0e66a
ramihaly <ramihaly@live.com>
2015-11-16 09:31:53 +0100

    Add feature to post to /entities endpoint

--- FileUpload/Default.aspx
+++ FileUpload/Default.aspx
@@ -3,12 +3,11 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
 	<div class="jumbotron">
-		<h1>DAM File Upload</h1>
-		<div>
-			<asp:Label runat="server" id="KeywordsLabel" Text="Enter some semi-colon (;) separated keywords:" />
-			<asp:TextBox runat="server" id="Keywords" />
-		</div>
-		<asp:FileUpload runat="server" id="FileUploadControl"/>
-		<asp:Button runat="server" id="UploadButton" Text="Upload" OnClick="UploadButton_Click" />
+	    <div>
+		    <asp:Label runat="server" id="KeywordsLabel" Text="Keywords:" />
+		    <asp:TextBox runat="server" id="Keywords" />
+	    </div>
+	    <asp:FileUpload runat="server" id="FileUploadControl"/>
+	    <asp:Button runat="server" id="UploadButton" Text="Upload" OnClick="UploadButton_Click" />
 	</div>
 </asp:Content>
--- FileUpload/Default.aspx.cs
+++ FileUpload/Default.aspx.cs
@@ -13,57 +13,117 @@ using System.Collections;
 
 namespace WebApplication1
 {
-	public partial class _Default : Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			var request = Helper.CreateRESTRequest("GET", "?comp=list");
-			var response = request.GetResponse() as HttpWebResponse;
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var request = Helper.CreateRESTRequest("GET", "?comp=list");
+            var response = request.GetResponse() as HttpWebResponse;
 
-			using (Stream stream = response.GetResponseStream())
-			{
-				var reader = new StreamReader(stream, Encoding.UTF8);
-				var responseString = reader.ReadToEnd();
-			}
-		}
+            if (response == null)
+            {
+                return;
+            }
+
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                {
+                    return;
+                }
+
+                var reader = new StreamReader(stream, Encoding.UTF8);
+                var responseString = reader.ReadToEnd();
+            }
+        }
 
 
-		protected void UploadButton_Click(object sender, EventArgs e)
-		{
-			if (!FileUploadControl.HasFile)
-			{
-				Debug.WriteLine("Empty file");
-				return;
-			}
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (!this.FileUploadControl.HasFile)
+            {
+                Debug.WriteLine("Empty file");
+                return;
+            }
 
-			try
-			{
-				string filename = Path.GetFileName(FileUploadControl.FileName);
-				Debug.WriteLine("File name: " + FileUploadControl.FileName.ToString());
-				var byteString = System.Text.Encoding.UTF8.GetString(FileUploadControl.FileBytes);
-				var fileContentStream = FileUploadControl.FileContent;
-				Debug.WriteLine("Keywords: " + Keywords.Text);
+            try
+            {
+                var filename = Path.GetFileName(this.FileUploadControl.FileName);
+                Debug.WriteLine("File name: " + filename);
+                var byteString = Encoding.UTF8.GetString(this.FileUploadControl.FileBytes);
+                //var fileContentStream = this.FileUploadControl.FileContent;
+                Debug.WriteLine("Keywords: " + this.Keywords.Text);
 
-				var headers = new SortedList<string, string>();
-				headers.Add("x-ms-blob-type", "BlockBlob");
-				var request = Helper.CreateRESTRequest("PUT", "/public/" + FileUploadControl.FileName, byteString, headers);
-				var response = request.GetResponse() as HttpWebResponse;
+                // POST to /entities endpoint
+                PostEntity(filename, this.Keywords.Text);
 
-				if (response.StatusCode == HttpStatusCode.Created)
-				{
-					Debug.WriteLine("Blob created");
-				}
+                // POST to /upload endpoint
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Could not upload file: " + ex.Message);
+            }
+        }
 
-				using (Stream stream = response.GetResponseStream())
-				{
-					var reader = new StreamReader(stream, Encoding.UTF8);
-					var responseString = reader.ReadToEnd();
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine("Could not upload file: " + ex.Message);
-			}
-		}
-	}
+        private static void PutBlob_Rest(string filename, string byteString)
+        {
+            // REST API call
+            var headers = new SortedList<string, string>
+                {
+                    { "x-ms-blob-type", "BlockBlob" }
+                };
+            var request = Helper.CreateRESTRequest("PUT", "/public/" + filename, byteString, headers);
+            var response = request.GetResponse() as HttpWebResponse;
+
+            if (response != null && response.StatusCode == HttpStatusCode.Created)
+            {
+                Debug.WriteLine("Blob created");
+            }
+
+            if (response == null)
+            {
+                return;
+            }
+
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                {
+                    return;
+                }
+
+                var reader = new StreamReader(stream, Encoding.UTF8);
+                var responseString = reader.ReadToEnd();
+            }
+        }
+
+        private static void PostEntity(string filename, string keywords)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                client.Headers[HttpRequestHeader.AcceptCharset] = "UTF-8";
+                client.Headers[HttpRequestHeader.UserAgent] = "Fiddler";
+
+                var values = new NameValueCollection();
+                values["Id"] = Guid.NewGuid().ToString();
+                values["Name"] = filename;
+                values["ContentUrl"] = AzureCredentials.Endpoint + "/public/" + filename;
+
+                var keywordsSplit = keywords.Split(';');
+                var keywordsJson = new StringBuilder();
+                keywordsJson.Append("[");
+                for (var i = 0; i < keywordsSplit.Length - 1; ++i)
+                {
+                    keywordsJson.Append(@"""" + keywordsSplit[i] + @"""" + ", ");
+                }
+                keywordsJson.Append(@"""" + keywordsSplit[keywordsSplit.Length - 1] + @"""" + "]");
+                var jsonString = @"{""Id"": """ + values["Id"] + @""", ""Name"": """ + values["Name"] + @""", ""Keywords"": " + keywordsJson + @", ""References"": [] }";
+                var response = client.UploadString("http://dam-ent-20151113.azurewebsites.net/digitalassets/entities/7fccfc4a5a10457f89d0ce5ca33c0f58/Files", jsonString);
+                Debug.WriteLine(response);
+            }
+        }
+
+
+    }
 }
