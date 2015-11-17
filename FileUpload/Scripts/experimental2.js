a7ec8abf97dd8a7b0219cda34f762705d27f7826
ramihaly <ramihaly@live.com>
2015-11-17 22:21:30 +0100

    Change $.post to $.ajax

--- FileUpload/Scripts/experimental2.js
+++ FileUpload/Scripts/experimental2.js
@@ -5,36 +5,36 @@ function S4() {
 }
  
 $(document).ready(function() {
-	$("#upload").on("click", function() {
-		var keywords = $("#keywords").val();
-		var fileInput = $("#picker")[0];
-		if (fileInput.files.length < 1) {
-			alert("Did not select any files");
-		}
-		var fileName = fileInput.files[0].name;
-		var fileGuid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    $("#upload").on("click", function (event) {
+        event.preventDefault();
 
-		var data = { 
-				"Id": fileGuid,
-				"Name": fileName,
-				"ContentUrl":"http://damtest1.blob.core.windows.net/public/" + fileName.split(".")[0],
-				"ParentFolderId":"00000000-0000-0000-0000-000000000000",
-				"Keywords": keywords.split(";")
-  		};
-  		$.post(entitiesEndpointUrl, data);
-  		/*
-  		$.ajax({
-		    type: 'POST',
-		    url: entitiesEndpointUrl,
-		    data: data,
-		    dataType: 'json',
-		    success: function(responseData, textStatus, jqXHR) {
-		        alert("POST successful");
-		    },
-		    error: function (responseData, textStatus, errorThrown) {
-		        alert('POST failed.');
-		    }
-		});
-*/
-	});
+        var keywords = $("#keywords").val();
+        var fileInput = $("#picker")[0];
+        if (fileInput.files.length < 1) {
+            alert("Did not select any files");
+        }
+        var fileName = fileInput.files[0].name;
+        var fileGuid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+
+        var data = { 
+                "Id": fileGuid,
+                "Name": fileName,
+                "ContentUrl":"http://damtest1.blob.core.windows.net/public/" + fileName.split(".")[0],
+                "ParentFolderId":"00000000-0000-0000-0000-000000000000",
+                "Keywords": keywords.split(";")
+        };
+        $.ajax({
+            type: "POST",
+            url: entitiesEndpointUrl,
+            data: data,
+            crossDomain: true,
+            dataType: "json",
+            success: function(responseData, textStatus, jqXHR) {
+                alert("POST successful");
+            },
+            error: function (responseData, textStatus, errorThrown) {
+                alert("POST failed.");
+            }
+        });
+    });
 });
