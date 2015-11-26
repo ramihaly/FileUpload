129e5f44612c4168f75d52ce129e75917a2eefb5
Razvan Mihalyi <ramihaly@microsoft.com>
2015-11-26 14:40:12 +0100

    Add HelperSaS class

--- FileUpload/HelperSas.cs
+++ FileUpload/HelperSas.cs
@@ -16,58 +16,81 @@ using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace WebApplication1
 {
-    public class HelperSas
-    {
-        public static string GetToken()
-        {
-            var storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", AzureCredentials.StorageAccountName, AzureCredentials.StorageAccountKey);
-            var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+	public class HelperSas
+	{
+		public static string GetToken()
+		{
+			var storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", AzureCredentials.StorageAccountName, AzureCredentials.StorageAccountKey);
+			var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
 
-            // Create the blob client object.
-            var blobClient = storageAccount.CreateCloudBlobClient();
+			// Create the blob client object.
+			var blobClient = storageAccount.CreateCloudBlobClient();
 
-            // Get a reference to the container for which shared access signature will be created.
-            var container = blobClient.GetContainerReference("private");
-            container.CreateIfNotExists();
+			// Get a reference to the container for which shared access signature will be created.
+			var container = blobClient.GetContainerReference("private");
+			container.CreateIfNotExists();
 
-            // Get the current permissions for the blob container.
-            var blobPermissions = container.GetPermissions();
+			// Get the current permissions for the blob container.
+			var blobPermissions = container.GetPermissions();
 
-            // Clear the container's shared access policies to avoid naming conflicts.
-            blobPermissions.SharedAccessPolicies.Clear();
+			// Clear the container's shared access policies to avoid naming conflicts.
+			blobPermissions.SharedAccessPolicies.Clear();
 
-            // The new shared access policy provides read/write access to the container for 24 hours.
-            blobPermissions.SharedAccessPolicies.Add("mypolicy", new SharedAccessBlobPolicy()
-            {
-                // To ensure SAS is valid immediately, don’t set the start time.
-                // This way, you can avoid failures caused by small clock differences.
-                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(15),
-                Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Create | SharedAccessBlobPermissions.Add
-            });
+			// The new shared access policy provides read/write access to the container for 24 hours.
+			blobPermissions.SharedAccessPolicies.Add("mypolicy-read", new SharedAccessBlobPolicy()
+			{
+				// To ensure SAS is valid immediately, don’t set the start time.
+				// This way, you can avoid failures caused by small clock differences.
+				SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(15),
+				Permissions = SharedAccessBlobPermissions.Read
+			});
 
-            // The public access setting explicitly specifies that 
-            // the container is private, so that it can't be accessed anonymously.
-            blobPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
+			blobPermissions.SharedAccessPolicies.Add("mypolicy-write", new SharedAccessBlobPolicy()
+			{
+				// To ensure SAS is valid immediately, don’t set the start time.
+				// This way, you can avoid failures caused by small clock differences.
+				SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(15),
+				Permissions = SharedAccessBlobPermissions.Write
+			});
 
-            // Set the new stored access policy on the container.
-            container.SetPermissions(blobPermissions);
+			// The public access setting explicitly specifies that 
+			// the container is private, so that it can't be accessed anonymously.
+			blobPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
 
-            // Get the shared access signature token to share with users.
-            return container.GetSharedAccessSignature(new SharedAccessBlobPolicy(), "mypolicy");
-        }
+			// Set the new stored access policy on the container.
+			container.SetPermissions(blobPermissions);
 
-        public static void UploadFile(Stream fileStream)
-        {
-            var token = GetToken();
-            var uri = new Uri(string.Format("https://{0}.blob.core.windows.net/private/{1}", AzureCredentials.StorageAccountName, Guid.NewGuid().ToString()));
+			CalculateContainerBytes(container);
 
-            // Create credentials with the SAS token. The SAS token was created in previous example.
-            var credentials = new StorageCredentials(token);
+			// Get the shared access signature token to share with users.
+			return container.GetSharedAccessSignature(new SharedAccessBlobPolicy(), "mypolicy");
+		}
 
-            // Create a new blob.
-            var blob = new CloudBlockBlob(uri, credentials);
+		public static void CalculateContainerBytes(CloudBlobContainer container)
+		{
+			var containerSizeInBytes = 48 + Guid.NewGuid().ToString().Length * 2;
+			foreach (var metadata in container.Metadata)
+			{
+				containerSizeInBytes += 3 + metadata.Key.Length + metadata.Value.Length;
+			}
 
-            blob.UploadFromStream(fileStream);
-        }
-    }
+			containerSizeInBytes += container.GetPermissions().SharedAccessPolicies.Count * 512;
+
+			Debug.WriteLine("Container " + container.Name + " size (kb): " + containerSizeInBytes / 1024.0);
+		}
+
+		public static void UploadFile(Stream fileStream)
+		{
+			var token = GetToken();
+			var uri = new Uri(string.Format("https://{0}.blob.core.windows.net/private/{1}", AzureCredentials.StorageAccountName, Guid.NewGuid().ToString()));
+
+			// Create credentials with the SAS token. The SAS token was created in previous example.
+			var credentials = new StorageCredentials(token);
+
+			// Create a new blob.
+			var blob = new CloudBlockBlob(uri, credentials);
+
+			blob.UploadFromStream(fileStream);
+		}
+	}
 }
